694ade94d0ff2c561ad09e7cdacf855b7e3d1a18
kirah-cox <kirahanncox@gmail.com>
2025-02-07 15:52:37 -0700

    Add project files.

---
+++ ConsoleApp16/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTools
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string FavoriteColor { get; set; }
+
+        public Person(string name, int age, string favoriteColor)
+        {
+            Name = name;
+            Age = age;
+            FavoriteColor = favoriteColor;
+        }
+    }
+
+    public class Tools
+    {
+        public static void Greet(Person person)
+        {
+            Console.WriteLine($"Hi, my name is {person.Name}, I am {person.Age} years old, and my favorite color is {person.FavoriteColor}.");
+        }
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyTools;
+
+public class Program
+{
+    static void Main()
+    {
+        Person person1 = new Person("Elpheba", 22, "Green");
+        Person person2 = new Person("Galinda", 21, "Pink");
+
+        Tools.Greet(person1);
+        Tools.Greet(person2);
+    }
+}
